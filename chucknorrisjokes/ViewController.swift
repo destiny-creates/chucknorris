6445c464f5b3c61fa006cdb7b795994bb91ab0b2
destiny creates <dronesforaliving@gmail.com>
2018-07-30 11:56:17 -0400

    added in  pods and created UI

---
+++ chucknorrisjokes/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  chucknorrisjokes
+//
+//  Created by Chris Cook on 7/30/18.
+//  Copyright © 2018 destinycreates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var factLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func factButtonTapped(_ sender: Any) {
+    }
+    
+
+}
+
